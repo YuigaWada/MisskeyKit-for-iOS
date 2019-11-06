a12a7448704285c01ed89e44484bce775c7941b3
YuigaWada <yuigawada@gmail.com>
2019-11-07 01:05:07 +0900

    Make sure to recieve error sent by server

--- MisskeyKit/Utilities/MisskeyError.swift
+++ MisskeyKit/Utilities/MisskeyError.swift
@@ -9,12 +9,21 @@
 
 
 public class MisskeyError: Codable {
-    let message, code, id: String?
+    
+    let error: Details?
+    
+    public class Details: Codable  {
+    let message, code, id, kind: String?
+    }
     
     static func checkNative(rawJson: String, _ nonNativeError: String)-> Error {
         
         if let error = rawJson.decodeJSON(MisskeyError.self) {
-            return NSError(domain: error.message!, code: -1, userInfo: nil)
+            guard let details = error.error, let errorMessage = details.message else {
+                 return NSError(domain: "Error sent by server has been recieved but MisskeyKit cannot decode this json.", code: -1, userInfo: nil)
+            }
+            
+            return NSError(domain: errorMessage, code: -1, userInfo: nil)
         }
         
         return NSError(domain: nonNativeError, code: -1, userInfo: nil)
