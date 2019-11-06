b13c56632d1aca72ae819adc3d6e17fe75eb3609
YuigaWada <yuigawada@gmail.com>
2019-11-06 23:00:01 +0900

    Fix some typos about api url

--- MisskeyKit/APIs/Wrapper/HTTP/Search.swift
+++ MisskeyKit/APIs/Wrapper/HTTP/Search.swift
@@ -42,7 +42,7 @@ extension MisskeyKit {
                           "userId":userId] as [String : Any]
             
             params = params.removeRedundant()
-            MisskeyKit.handleAPI(needApiKey: true, api: "users/search", params: params, type: [UserModel].self) { users, error in
+            MisskeyKit.handleAPI(needApiKey: true, api: "notes/search", params: params, type: [UserModel].self) { users, error in
                 
                 if let error = error  { callback(nil, error); return }
                 guard let users = users else { callback(nil, error); return }
@@ -64,7 +64,7 @@ extension MisskeyKit {
                           "untilId": untilId] as [String : Any]
             
             params = params.removeRedundant()
-            MisskeyKit.handleAPI(api: "notes", params: params, type: [NoteModel].self) { posts, error in
+            MisskeyKit.handleAPI(api: "notes/search-by-tag", params: params, type: [NoteModel].self) { posts, error in
                 if let error = error  { callback(nil, error); return }
                 guard let posts = posts else { callback(nil, error); return }
                 
